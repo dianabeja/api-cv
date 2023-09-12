99ca583aa98ea99f9d1a58573ec5fd49c7cec087
Diana Bautista <bagdiana03@gmail.com>
2023-09-12 15:09:49 -0600

    cambios

--- index.js
+++ index.js
@@ -14,9 +14,31 @@ app.get('/', (req, res, next) => {
 
 
 app.get('/header', (req, res, next) => {
-  res.sendfile("assets/header.json");
+	res.sendfile("assets/header.json");
 });
 
-app.listen(port,  () => 
+app.get('/workexperience', (req, res, next) => {
+	res.sendfile("assets/work.json");
+});
+
+app.get('/skills', (req, res, next) => {
+	res.sendfile("assets/skills.json");
+});
+
+app.get('/certificados', (req, res, next) => {
+	res.sendfile("assets/certiicados.json");
+});
+
+app.get('/education', (req, res, next) => {
+	res.sendfile("assets/education.json");
+});
+
+app.get('/interest', (req, res, next) => {
+	res.sendfile("assets/interest.json");
+});
+
+
+app.listen(port, () =>
 	console.log('listening on port ' + port
-));
+	));
+
