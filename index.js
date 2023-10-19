224a13d5747507344a63b57136e428ec812cf1f5
Diana Bautista <bagdiana03@gmail.com>
2023-10-19 11:38:00 -0600

    test 1 #8

--- index.js
+++ index.js
@@ -46,6 +46,22 @@ app.get('/size', (req, res, next) => {
 	res.sendfile("assets/1a_proxy_size.json");
 });
 
+app.get('/test1', (req, res, next) => {
+	res.sendfile("assets/3a_test1.json");
+});
+
+app.get('/test2', (req, res, next) => {
+	res.sendfile("assets/3a_test2.json");
+});
+
+app.get('/test3', (req, res, next) => {
+	res.sendfile("assets/3a_test3.json");
+});
+
+app.get('/test4', (req, res, next) => {
+	res.sendfile("assets/3a_test4.json");
+});
+
 app.listen(port, () =>
 	console.log('listening on port ' + port
 	));
